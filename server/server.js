5e71a574d5c5c6914ac74cda4447318281dfbf3a
Aman <amanabbansal@gmail.com>
2025-07-29 11:21:49 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start server
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
